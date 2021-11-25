3ee9f31f710d765dfd8bda186d12c4c1080e8235
Denis Butsky <nthbutsky@gmail.com>
2021-11-25 04:27:20 +0200

    add dev part

---
+++ public/js/slider.js
@@ -0,0 +1,13 @@
+// const allContainers = document.querySelectorAll(".container");
+// document.addEventListener("DOMContentLoaded", () => {
+//   allContainers.forEach((item) => {
+//     item.addEventListener("click", function (e) {
+//       if (e.target.nextElementSibling.style.display === "none") {
+//         e.target.nextElementSibling.style.display = "initial";
+//       } else {
+//         e.target.nextElementSibling.style.display = "none";
+//       }
+//     });
+//   });
+// });
+"use strict";
