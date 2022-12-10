39fd003c160e56c6e33efa49106f9099f0d1af13
Denis Butsky <butskyi@convidera.com>
2022-12-10 06:54:21 -0800

    deploy

--- public/js/gallery.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-document.addEventListener("DOMContentLoaded", function () {
-  const zooming = new Zooming({
-    bgColor: "rgba(0, 0, 0, 0.75)"
-  });
-  zooming.listen(".img-zoomable");
-});
--- src/gallery.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const zooming = new Zooming({
-    bgColor: "rgba(0, 0, 0, 0.75)",
-  });
-
-  zooming.listen(".img-zoomable");
-});
